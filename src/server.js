8434d0ef2f187810f350b0071c63d7750d01db4a
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-08-10 14:06:03 +0100

    add cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from "express"
 import listEndpoints from "express-list-endpoints"
 import authorsRouter from "./services/authors/index.js"
+import cors from 'cors'
 
 const server = express()
 
 const port = 3001
 
+server.use(cors())
 server.use(express.json())
 
 // *************** ROUTES *****************
